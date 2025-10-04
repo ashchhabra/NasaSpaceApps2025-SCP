1b3017890d0e9de44dd1bde112f3204d78e19851
m0Zahed <prettyflackotheg@gmail.com>
2025-10-04 12:33:41 -0400

    UI (initialisation): Generated the a React Typescript project using nextJS

---
+++ UI/frontend/payload/src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+      
+      </div>
+    </>
+  );
+}
+
+export const metadata = {
+  title: 'Payload',
+  description: 'Hello! Hope you\'re having a great day!',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
+
